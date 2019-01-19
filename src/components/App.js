45444c514be5637de2b647448b780ba3a9c0239d
Jordan Nieuwhof <jgnieuwhof@gmail.com>
2019-01-18 23:02:38 -0500

    background image

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,29 @@
 import React from 'react';
 import { Switch, Route } from 'react-router';
+import styled from '@emotion/styled';
+
+import background from 'img/background.jpg';
 
 import Header from 'components/Header';
 import Footer from 'components/Footer';
 import { Home, Contact } from 'components/pages';
-import { Div, Flex } from 'components/uikit';
+import { Flex } from 'components/uikit';
+
+const Content = styled(Flex)`
+  background: url(${background}) no-repeat center center fixed;
+  background-size: cover;
+`;
 
 const App = () => {
   return (
     <Flex flexDirection="column" height="100%">
       <Header />
-      <Div flexGrow={1} minHeight={1}>
+      <Content flexGrow={1} minHeight={1} p={4} justifyContent="center">
         <Switch>
           <Route exact path="/" render={x => <Home {...x} />} />
           <Route exact path="/contact" render={x => <Contact {...x} />} />
         </Switch>
-      </Div>
+      </Content>
       <Footer />
     </Flex>
   );
