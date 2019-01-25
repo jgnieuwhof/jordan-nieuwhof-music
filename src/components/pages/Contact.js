90790fd9fe0c74d16b52c653f98a83b8d737a0f0
Jordan Nieuwhof <jgnieuwhof@gmail.com>
2019-01-25 08:51:57 -0500

    added basic contact form

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { Form } from 'react-formio';
 
 import desert from 'img/desert.jpg';
 import Page from './Page';
+import { Div } from 'components/uikit';
 
 const Contact = () => {
-  return <Page background={desert}>Contact</Page>;
+  return (
+    <Page background={desert} header="Contact">
+      <Div>
+        Feel free to send an email to{' '}
+        <a href="mailto:jnieuwhofmusic@gmail.com">jnieuwhofmusic@gmail.com</a>,
+        or fill out the form below and I'll get back to you as soon as I can.
+      </Div>
+      <Div buffer>
+        <Form src="https://mvnlshtnphngfrw.form.io/contact-us" />
+      </Div>
+    </Page>
+  );
 };
 
 export default Contact;
