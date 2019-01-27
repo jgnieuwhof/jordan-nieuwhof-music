23f80e868f408e5748869dbf1485d6fa7dff8d01
Jordan Nieuwhof <jgnieuwhof@gmail.com>
2019-01-26 23:02:41 -0500

    unused

--- src/common/constants.js
+++ src/common/constants.js
@@ -1,10 +1,7 @@
 const {
-  REACT_APP_FB_APP_ID,
   REACT_APP_CONTENTFUL_ACCESS_TOKEN,
   REACT_APP_CONTENTFUL_SPACE
 } = process.env;
-
-export const fbAppId = REACT_APP_FB_APP_ID;
 
 export const contentfulAccessToken = REACT_APP_CONTENTFUL_ACCESS_TOKEN;
 
